54c046366fb42449273dc523d9455c8254cd024f
Ravinderjit <rjassal@intalio.com>
2014-03-04 12:18:49 +0530

    Fixed WF-1721

--- AbsenceRequest/src/main/gi/Forms/NotifyAbsenceRequest.gi/js/logic.js
+++ AbsenceRequest/src/main/gi/Forms/NotifyAbsenceRequest.gi/js/logic.js
@@ -1,1 +1,11 @@
 /* place JavaScript code here */
+
+var form=NotifyAbsenceRequest;
+
+function resizeLeaveList(){
+    var leaveList=form.getJSXByName("LeaveList");
+    var newHeight=(leaveList.getRecordIds().length+1)*leaveList.getRowHeight()+leaveList.getHeaderHeight();
+    leaveList.getParent().setHeight(newHeight).repaint();
+    leaveList.setHeight(newHeight).repaint();
+    leaveList.getParent().repaint();
+}
